e35b0c4d1ef57ae99fd1ec61841e62addd668241
Timo Gruwel <Timo.Gruwel@Digital-power.com>
2023-10-26 13:31:43 +0200

    Minor change main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
 from typing import List
 from quote_engine import ImportInterface, DocxImporter, CSVImporter, PDFImporter, TXTImporter
-from dog import Dog
+from quote_model import QuoteModel
 
 class Importer(ImportInterface):
     importers = [DocxImporter, CSVImporter, PDFImporter,TXTImporter]
 
     @classmethod
-    def parse(cls, path: str) -> List[Dog]:
+    def parse(cls, path: str) -> List[QuoteModel]:
         for importer in cls.importers:
             if importer.can_ingest(path):
                 return importer.parse(path)
